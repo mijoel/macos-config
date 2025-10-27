b4d7dea3b74393dc00c948697155c9b9d3c74079
Mihkel Jõela <mihkel.joela@gmail.com>
2025-10-27 11:37:24 +0200

    custom raycast script + bash_profile change

---
+++ raycast-custom-scripts/lõuna.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Lõuna
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author mihkel_joela
+# @raycast.authorURL https://raycast.com/mihkel_joela
+
+open https://rotermann.ee/tana-lounaks/
+
+
